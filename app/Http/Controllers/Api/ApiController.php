095b4f85c9b4781a66467f70b841fc6a4e832678
Cibulsky Dmitry <d.cibulsky@oyster-telecom.ru>
2020-04-17 03:04:59 +0300

    Add api routes and create api controller

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\FilterCityRequest;
+use App\Http\Requests\Api\FilterNameRequest;
+
+use App\User;
+
+class ApiController extends Controller
+{
+    /**
+     * @return User[]|\Illuminate\Database\Eloquent\Collection
+     */
+    public function index()
+    {
+        $users = User::all();
+
+        return $users;
+    }
+
+    public function name(FilterNameRequest $request)
+    {
+        $validated = $request->validated();
+        $users = User::orWhere('first_name' , 'like' , '%' . $validated['name'] . '%' )
+        ->orWhere('second_name' , 'like' , '%' . $validated['name'] . '%' )
+        ->orWhere('patronymic' , 'like' , '%' . $validated['name'] . '%' )
+        ->get();
+
+        return $users;
+    }
+
+    public function city(FilterCityRequest $request)
+    {
+        $validated = $request->validated();
+        $users = User::where('city_id', $validated['city_id'])->get();
+        return $users;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group([
+    'namespace' => 'Api',
+], function (){
+    Route::get('/users', 'ApiController@index');
+    Route::get('/users/name/', 'ApiController@name');
+    Route::get('/users/city/', 'ApiController@city');
 });
